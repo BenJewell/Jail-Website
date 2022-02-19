18421658e9fa99bb582d11e7b96df1bc08f2fcdf
BenJewell <bjewel@pointpark.edu>
2022-02-19 17:21:23 -0500

    Fixed wrong folder

--- Module6/app.js
+++ Module6/app.js
@@ -1,44 +1,53 @@
-const APIController = function () {
-    const clientId = "f73580adbc8d4eb3b983a308ca50b289";
-    const clientSecret = "d97f18e5cbc044d7980c38e7d535504a";
+const APIController = (function(){
+   const clientId = 'd174071615cf4e25a9935d59053bc454';
+   const clientSecret = '5f45fdae5f794eaaa0d7cd0ec4770427';
 
-    // private methods
-    const _getToken = async () => {
+   //private methods
+   const _getToken = async () => {
 
-        const result = await fetch("http://accounts.spotify.com/api/token" {
+        const result = await fetch('https://accounts.spotify.com/api/token', {
             method: 'POST',
-            headers {
-                'content-type:............' btoa
-            }
-        })
-    }
+            headers: {
+                'Content-Type' : 'application/x-www-form-urlencoded',
+                'Authorization' : 'Basic ' + btoa( clientId + ':' + clientSecret)
+            },
+            body: 'grant_type=client_credentials'
+        });
 
-    const _getGenres = async (token) => {
+            const data = await result.json();
+            return data.access_token;
 
-        const result = await fetch("" {
-            method: 'GET',
-            headers {
-                'content-type:............' btoa
-            }
-        })
-    }
+   }
 
-    const _getPlaylistByGenre = async (token, genreId) => {
+   const _getGenres = async (token) => {
+     
+       const result = await fetch('https://api.spotify.com/v1/browse/categories?locale=sv_US', {
+           method: 'GET',
+           headers: { 'Authorization' : 'Bearer ' + token}
+       });
 
-    }
+       const data = await result.json();
+       return data.categories.items;
 
-    const _getTracks = async (token, tracksEndPoint) => {
+   }
 
-    }
+   const _getPlaylistByGenre = async (token, genreId) => {
 
-    const _getTrack = async (token, tracksEndPoint) => {
+   }
 
-    }
+   const _getTracks = async (token, tracksEndPoint) => {
 
-    return {
-        getToken() {
-            return _getToken();
-        } // Need this for all of the constants that we have at the top.
-    }
+   }
 
-}
+   const _getTrack = async (token, tracksEndPoint) => {
+
+   }
+
+   return {
+       getToken() {
+           return _getToken();
+       }
+       //Need to do this for all of the constants that we have at the top
+   }
+
+})();
